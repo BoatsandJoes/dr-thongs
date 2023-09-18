b37efd75a1d3c371f9c7511775ff5f76d1072f8d
BoatsandJoes <joefried2012@gmail.com>
2023-09-18 11:10:27 -0500

    Added port to Lobby screen.

--- scenes/UI/Menus/Lobby/Lobby.gd
+++ scenes/UI/Menus/Lobby/Lobby.gd
@@ -2,6 +2,9 @@ extends CanvasLayer
 class_name Lobby
 
 var regex: RegEx
+var port: String
+var ip: String
+var hosting = false
 
 signal back
 
@@ -13,15 +16,34 @@ func _ready():
 	%IP.grab_focus.call_deferred()
 
 func _on_Host_pressed():
-	%HostJoin.visible = false
+	if %Port.text != null:
+		regex.compile("[0-9]?[0-9]?[0-9]?[0-9][0-9]")
+		var result = regex.search(%Port.text)
+		if result:
+			port = result.get_string()
+			hosting = true
+			openLobby()
 
 func _on_Join_pressed():
 	if %IP.text != null:
 		regex.compile("[1-2]?[0-9]?[0-9]\\.[1-2]?[0-9]?[0-9]\\.[1-2]?[0-9]?[0-9]\\.[1-2]?[0-9]?[0-9]")
 		var result = regex.search(%IP.text)
 		if result:
-			var ip = result.get_string()
-			%HostJoin.visible = false
+			regex.compile(":[0-9]?[0-9]?[0-9]?[0-9][0-9]")
+			var result2 = regex.search(%IP.text)
+			if result2:
+				ip = result.get_string()
+				port = result2.get_string().substr(1)
+				hosting = false
+				openLobby()
+
+func openLobby():
+	%HostJoin.visible = false
+	%Lobby.visible = true
+
+func closeLobby():
+	%Lobby.visible = false
+	%HostJoin.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -32,6 +54,6 @@ func _input(event):
 		if %HostJoin.visible:
 			emit_signal("back")
 		else:
-			%HostJoin.visible = true
+			closeLobby()
 	elif event.is_action_pressed("enter"):
 		_on_Join_pressed()
